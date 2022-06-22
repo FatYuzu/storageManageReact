72f376ad93b3c9f868c8358ff16dfc3b8b4012e7
FatYuzu <iswjh@foxmail.com>
2022-06-22 23:06:21 +0800

    解包前

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from 'antd';
+
+export default function App() {
+  return (
+    <div>
+        <Button type="primary">Primary Button</Button>
+    </div>
+  )
+}
---
+++ src/request/api.js
@@ -0,0 +1,3 @@
+import request from './request'
+
+export const xxApi = () => request.get('/xx')
